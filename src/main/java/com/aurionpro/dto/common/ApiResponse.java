afce79e9985906105703e4127f60e85a80547b98
adityaellhe <adityaswabhav@gmail.com>
2025-10-05 18:22:41 +0530

    sucesss

--- src/main/java/com/aurionpro/dto/common/ApiResponse.java
+++ src/main/java/com/aurionpro/dto/common/ApiResponse.java
@@ -5,7 +5,7 @@ import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class ApiResponse<T> {
-  private boolean successfull;
+  private boolean successs;
   private String message;
   private T data;
 }
